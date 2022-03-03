cf73df145c8353136738555286b7399249e11722
YUZEJIA <yuzejia@mabangerp.com>
2022-03-03 18:00:19 +0800

    提交

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,46 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React, { useEffect, useState, ReactDOM } from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./index.module.css";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">hi</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      
-      </div>
-    </header>
-  );
+function HomepageHeader({ count }) {
+  let html = { __html: count };
+  return <div id="htmlS" dangerouslySetInnerHTML={html}></div>;
 }
 
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+function AddDom({ el, t }) {
+  let [txt, setTxt] = useState<string>(t);
+  // setTimeout(() => {
+  //   setTxt(txt + "333");
+  //   console.log(1111);
+  // }, 1000);
+  let e = React.createElement("h1", null, txt);
+  console.log(e);
+
+  return e;
+}
+
+const Home: React.FC = () => {
+  const str = "你好，YUZEJIA.";
+  let [index, setIndex] = useState<number>(0);
+  const [counts, setCount] = useState<string>(`<h1>${index}</h1>`);
+
+  let html = { __html: counts };
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
+      description="Description will go into a meta tag in <head />"
+    >
+      <AddDom el={"h1"} txt={"xxxxx"} />
       <main>
         <HomepageFeatures />
       </main>
     </Layout>
   );
-}
+};
+
+export default Home;
