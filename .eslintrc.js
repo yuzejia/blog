f4c09e475e4c618f3c206e02658ebc7f527189dd
于泽佳-Zejia <zejia.yu@d1m.cn>
2023-01-06 11:50:17 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
-    "env": {
+    root: true, // // 告诉eslint找当前配置文件不能往父级查找
+    "env": { // / 全局环境
         "browser": true,
-        "es2021": true
+        "es2021": true   // // 例子：这样设置之后，就支持新的 ES6 全局变量和类型
     },
-    "extends": [
+    "settings": {
+        "react": {
+          "version": "999.999.999"
+        }
+      },
+    "extends": [ // // 让 ESlint 添加规则模块
         "eslint:recommended",
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended"
@@ -11,15 +17,15 @@ module.exports = {
     "overrides": [
     ],
     "parser": "@typescript-eslint/parser",
-    "parserOptions": {
+    "parserOptions": { // // 优先级低于parse的语法解析配置
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "plugins": [
+    "plugins": [ // // 使用第三方插件检查规则
         "react",
         "@typescript-eslint"
     ],
-    "rules": {
+    "rules": { //  // 规则，手动自定义代码规范
         "no-empty-function": "off",
         "@typescript-eslint/no-empty-function": "off"
     }
