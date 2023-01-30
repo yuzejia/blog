21b97a3f5e08a101a088db6a7edc60e5392dffc7
于泽佳-Zejia <zejia.yu@d1m.cn>
2023-01-30 15:51:59 +0800

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,11 +31,44 @@ export default class Home extends React.Component {
 
   async componentDidMount() {
     const home = document.querySelector("#htmlS");
-    // await writeOneByOne("嗨哟。", "h2", home, { interval: 150, sleep: 300 })
-    // await writeOneByOne("我是 YUZEJIA，", "h4", home, { interval: 150, sleep: 300})
-    // await writeOneByOne("一名前端开发工程师~。", "p", home, { sleep: 400 })
+    await this.writeOneByOne("嗨哟。", "h2", home, { interval: 150, sleep: 300 })
+    await this.writeOneByOne("我是 YUZEJIA，", "h4", home, { interval: 150, sleep: 300})
+    await this.writeOneByOne("一名前端开发工程师~。", "p", home, { sleep: 400 })
+  }
+  writeOneByOne(text: string | string[], dom: string, container: Element, opt: { interval?: unknown; sleep: unknown; }) {
+    const el = document.createElement(dom);
+    const bar = document.createElement("span");
+    bar.className = "write-vertical-bar";
+    el.appendChild(bar);
+    container.appendChild(el);
+  
+    let timer = null, currentIndex = 0
+    const sleep = opt.sleep || 0;
+  
+    return new Promise((resolve) => {
+      function next() {
+        const textNode = document.createTextNode(text[currentIndex]);
+        el.insertBefore(textNode, bar);
+        currentIndex++;
+  
+        if (currentIndex < text.length) {
+          timer = setTimeout(() => {
+            next();
+          }, 100);
+        } else {
+          timer = setTimeout(() => {
+            el.removeChild(bar);
+            clearTimeout(timer);
+            resolve("");
+          }, sleep);
+        }
+      }
+  
+      next();
+    });
   }
 }
+
 
 function HomepageHeader() {
   return (
